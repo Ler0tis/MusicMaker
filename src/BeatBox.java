63de63e52a5e1621dc72366de6a90d18c57aa6cc
Leroy <leroyotis@live.nl>
2021-05-03 14:26:20 +0200

    Added local project

---
+++ src/BeatBox.java
@@ -0,0 +1,21 @@
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.swing.*;
+import java.util.ArrayList;
+
+
+
+public class BeatBox {
+
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checkboxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame theFrame;
+
+
+    public static void main(String[] args) {
+
+    }
+}
