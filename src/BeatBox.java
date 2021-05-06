8260047f1e9daf74049fbca9c98005f22278cf32
Leroy <leroyotis@live.nl>
2021-05-06 17:13:59 +0200

    Code to save and restore patterns in BeatBox

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -2,9 +2,12 @@
 import javax.sound.midi.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
 import java.awt.event.*;
 import java.awt.*;
+import java.io.*;
 
 
 
@@ -191,5 +194,57 @@ public class BeatBox {
             return event;
 
         }
+
+        public class MySendListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent a) {
+
+            boolean[] checkboxState = new boolean[256];
+
+            for (int i = 0; i < 256; i++) {
+
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (check.isSelected()) {
+                    checkboxState[i] = true;
+                }
+            }
+
+            try {
+                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkboxState);
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        }
+
+        public class MyReadInListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent a) {
+            boolean[] checkboxState = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            } catch (Exception ex) {ex.printStackTrace(); }
+
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (checkboxState[i]) {
+                    check.setSelected(true);
+                } else {
+                    check.setSelected(false);
+                }
+            }
+
+            sequencer.stop();
+            buildTrackAndStart();
+        }
+
+
+        }
+
     }
 
